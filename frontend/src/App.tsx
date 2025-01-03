b616d9f06bdd0f2fb1170f8ddee9ec034f1f28c4
danelsender <de296@exeter.ac.uk>
2025-01-03 19:47:33 +0000

    Features added: - Settings button moved to nav bar
    Next steps:
    - Get better centering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,31 +6,31 @@ import EditEntry from "./routes/EditEntry";
 import NewEntry from "./routes/NewEntry";
 import { EntryProvider } from "./utilities/globalContext";
 
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
 
 import { Switch } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 // Transition component for the dialog
-import { TransitionProps } from '@mui/material/transitions';
+import { TransitionProps } from "@mui/material/transitions";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function App() {
   // state to toggle the dark mode
-  const [toggleDarkMode, setToggleDarkMode] = useState(false); 
+  const [toggleDarkMode, setToggleDarkMode] = useState(false);
 
   const toggleDarkTheme = () => {
     document.documentElement.classList.toggle("dark");
@@ -46,36 +46,12 @@ export default function App() {
   const handleClose = () => {
     setOpen(false);
   };
-  
 
   return (
     <div
       className="text-black dark:text-white dark:bg-black"
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      style={{ display: "flex", flexDirection: "column" }}
     >
-      {/* // Adding the switch button
-      <Switch onChange={toggleDarkTheme} /> */}
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Settings
-      </Button>
-      <Dialog
-        PaperProps={{className: "dark:bg-black dark:text-white"}}
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle >{"Settings"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Dark Mode <Switch onChange={toggleDarkTheme} />
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
       <section>
         <Router>
           <EntryProvider>
@@ -91,47 +67,3 @@ export default function App() {
     </div>
   );
 }
-
-
-
-
-
-// export default function AlertDialogSlide() {
-//   const [open, setOpen] = React.useState(false);
-
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
-
-//   const handleClose = () => {
-//     setOpen(false);
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <Button variant="outlined" onClick={handleClickOpen}>
-//         Slide in alert dialog
-//       </Button>
-//       <Dialog
-//         open={open}
-//         TransitionComponent={Transition}
-//         keepMounted
-//         onClose={handleClose}
-//         aria-describedby="alert-dialog-slide-description"
-//       >
-//         <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-//         <DialogContent>
-//           <DialogContentText id="alert-dialog-slide-description">
-//             Let Google help apps determine location. This means sending anonymous
-//             location data to Google, even when no apps are running.
-//           </DialogContentText>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button onClick={handleClose}>Disagree</Button>
-//           <Button onClick={handleClose}>Agree</Button>
-//         </DialogActions>
-//       </Dialog>
-//     </React.Fragment>
-//   );
-// }
-
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,67 @@
 import { NavLink } from "react-router-dom";
 
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { Switch } from "@mui/material";
+
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function NavBar() {
+
+   const [toggleDarkMode, setToggleDarkMode] = useState(false);
+   
+     const toggleDarkTheme = () => {
+       document.documentElement.classList.toggle("dark");
+       setToggleDarkMode(!toggleDarkMode);
+     };
+   
+     const [open, setOpen] = React.useState(false);
+   
+     const handleClickOpen = () => {
+       setOpen(true);
+     };
+   
+     const handleClose = () => {
+       setOpen(false);
+     };
   return (
-    <nav className="flex justify-center gap-5">
+    <nav className="flex justify-between items-center p-4">
+      <div className="navbar-left">
+      <Button className="m-3 p-4" onClick={handleClickOpen}>
+              Settings
+            </Button>
+            <Dialog
+              open={open}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={handleClose}
+              aria-describedby="alert-dialog-slide-description"
+            >
+              <DialogTitle>{"Settings"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                  Dark Mode <Switch onChange={toggleDarkTheme} />
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Close</Button>
+              </DialogActions>
+            </Dialog>
+      </div>
+      <div className="navbar-center">
       <NavLink
         className="m-3 p-4 text-xl bg-blue-400 dark:bg-green-800 hover:bg-blue-500 dark:hover:bg-green-600 rounded-md font-medium text-white"
         to={"/"}
@@ -15,6 +74,8 @@ export default function NavBar() {
       >
         New Entry
       </NavLink>
+      </div>
+      <div className="navbar-right"></div>
     </nav>
   );
 }
